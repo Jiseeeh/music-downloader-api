7baec666e437a5a8d34313607085d074b670f29e
Jiseeeh <johncarlo.camara1@gmail.com>
2023-08-06 15:03:41 +0800

    feat: add app handlers

--- music-downloader-api/server.ts
+++ music-downloader-api/server.ts
@@ -1,10 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
 
 import { PORT } from "@config";
+import { ErrorMiddleware } from "src/middlewares/error.middleware";
+import { downloadRouter } from "src/routes/download.route";
+import { downloadLimiter } from "@limiters/download.limiter";
 
 const app = express();
 dotenv.config();
+
+// middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.json());
+
+// routes
+app.use("/api/download", downloadRouter);
+// app.use("/api/download", downloadLimiter, downloadRouter);
+
+// error handler
+app.use(ErrorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`\nRunning at http://localhost:${PORT}`);
