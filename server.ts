efebd7d1a6e742e79089dc846e6703c5b8fdc055
Jiseeeh <johncarlo.camara1@gmail.com>
2023-08-09 23:07:02 +0800

    feat: add cors

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
+import cors from "cors";
 
 import { PORT } from "@config";
 import { ErrorMiddleware } from "src/middlewares/error.middleware";
@@ -12,6 +13,11 @@ const app = express();
 dotenv.config();
 
 // middleware
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
